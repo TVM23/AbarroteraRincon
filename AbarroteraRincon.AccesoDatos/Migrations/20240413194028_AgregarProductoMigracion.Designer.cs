7bb56103dc65ce35c6f0b3bf057e8ef2d75c1ec0
TVM23 <tadeo23manzo@outlook.com>
2024-04-18 12:48:26 -0600

    Trabajando Producto

--- AbarroteraRincon.AccesoDatos/Migrations/20240413194028_AgregarProductoMigracion.Designer.cs
+++ AbarroteraRincon.AccesoDatos/Migrations/20240413194028_AgregarProductoMigracion.Designer.cs
@@ -4,6 +4,7 @@ using AbarroteraRincon.AccesoDatos.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AbarroteraRincon.AccesoDatos.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240413194028_AgregarProductoMigracion")]
+    partial class AgregarProductoMigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AbarroteraRincon.AccesoDatos/Repositorio/IRepositorio/IProductoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using AbarroteraRincon.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbarroteraRincon.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IProductoRepositorio : IRepositorio<Producto>
+    {
+        void Actualizar(Producto producto);
+    }
+}
